12e4e9ab44fda463d8c9853fc6ac4959bd1e42b5
MrKolev <100051403+MrKolev@users.noreply.github.com>
2023-04-07 18:10:15 +0300

    add Event onClick

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,15 +1,25 @@
+import { useState } from 'react';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate.jsx';
 
 import './ExpenseItem.css'
+
 function ExpenseItem(props) {
+
+   const [title, satTitle] = useState(props.title);
+   
+    const clickBtn = () => {
+        satTitle("Update!");
+    }
+
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
-            <div className="expense-item__description">
-                <h2>{props.title}</h2>
+            <div  className="expense-item__description">
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
+            <button onClick={clickBtn}>chenge</button>
         </Card>
     )
 }
